8c8c4b303ada1a6540e344ac8734bbc607c4bb90
cesaredm <cesaredmaradiaga@outlook.com>
2020-01-29 19:43:49 -0600

    encabezado de titulo y detalles de card

---
+++ js/opcionCarrusel.js
@@ -0,0 +1,8 @@
+$('.carousel').carousel({
+    //intervalo en milesegundos para la intecambiar de imagenes o false para que no sea automatico
+    interval: false,
+    //para pausar el tiempo cuando el cursor esta fuera de slide y renudar cuando vuelve al slide true o false
+    pause:false,
+    //para hacer ciclar al carrusel continuamente true o false
+    wrap:false
+});
